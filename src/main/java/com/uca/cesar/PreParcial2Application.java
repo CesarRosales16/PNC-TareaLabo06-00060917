225d597c5d90481b4088faa3c94c3c19b1602a60
Cesar Rosales <00060917@uca.edu.sv>
2020-05-25 09:18:51 -0600

    base project

---
+++ src/main/java/com/uca/cesar/PreParcial2Application.java
@@ -0,0 +1,13 @@
+package com.uca.cesar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreParcial2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreParcial2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/uca/cesar/PreParcial2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.cesar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreParcial2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
